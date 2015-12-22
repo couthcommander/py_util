b5d4342fdde0f6cfd17520bf2919b892454ba5ac
beckca <cole.beck@vanderbilt.edu>
2015-12-22 14:08:51 -0600

    new adherence script

--- checkPersistence.py
+++ checkPersistence.py
@@ -22,125 +22,114 @@ class Nothing():
     def add(self):
         pass
 
-class dsihFile(file):
-    def __init__(self, name, percent):
-        file.__init__(self, name)
-        header = self.readline().rstrip().split(',')
-        cols = ['scrssn', 'svc_dteoffset', 'DSIHpre'+str(percent), 'DSIHpost'+str(percent)]
-        if False in [i in header for i in cols]:
-            print "DSIH file [%s] is not valid" % (name)
-            sys.exit()
-        self.colOrder = [header.index(i) for i in cols]
+def mysort(a,b):
+    return cmp(a[0],b[0])
 
-    def read(self):
-        line = self.readline().rstrip().split(',')
-        if len(line) > 1:
-            return [int(line[i]) for i in self.colOrder]
-        else:
-            return []
+def calcDSIH(dat, stockpile, vfun, fh, delim, skip=False):
+    if len(dat) < 1:
+        return(None, [])
+    curid = dat[0]
+    dat = [vfun(dat)]
+    line = fh.readline().rstrip().split(delim)
+    while len(line) > 1 and line[0] == curid:
+        dat.append(vfun(line))
+        line = fh.readline().rstrip().split(delim)
+    if skip:
+        return(None, line)
+    dat.sort(mysort)
+    res = [[dat[i][0],0,0] for i in range(len(dat))]
+    res[0][2] = dat[0][1]
+    for i in range(1, len(dat)):
+        res[i][2] = dat[i][1]
+        dayspassed = dat[i][0] - dat[i-1][0]
+        # previous DS - days passed
+        if dayspassed > 0:
+            pre = res[i-1][2] - dayspassed
+            if pre > 0:
+                res[i][2] += int(round(pre * stockpile/100.0))
+            res[i][1] = pre
+    return(res, line)
 
 # measure compliance
-def calcDropout(start, dates, gap):
-    alldates = dates.keys()
-    if start not in alldates:
-        return '.'
-    alldates.sort()
-    lastdate = start
-    for i in range(alldates.index(start)+1, len(dates)):
-        if int(dates[alldates[i]][0]) + gap >= 0:
-            lastdate = alldates[i]
+def calcDropout(start, dat, gap):
+    lastdate = 0
+    for i in range(len(dat)):
+        if dat[i][0] <= start or dat[i][1] + gap >= 0:
+            lastdate = i
         else:
             break
-    #the post pill quantity for current prescription
-    pills = dates[lastdate][1]
-    # dropout date is last prescription date PLUS the DSIHpost on that day
-    # dropout = lastdate + pills
-    return [lastdate, pills]
-
-def readMain(line):
-    return [int(line[0]), int(line[2])]
-
-def missingRow(line):
-    return line + delim_val + missing_val + delim_val + missing_val + "\n"
+    return dat[lastdate]
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("datafile", help='delimited file, should contain PTID|DATE|OFFSET|DRUGNAME|ETC')
-    parser.add_argument("DSIHfile", help='DSIH file, should contain PTID|DATE|OFFSET|PRE|POST')
+    parser.add_argument("infile", help='delimited file, should contain PTID|DATE|OFFSET|DRUGNAME|ETC')
+    parser.add_argument("rxfile", help='delimited file, should contain PTID|DATE|OFFSET|DAYSUPPLY')
     parser.add_argument("outputfile", help='output file, checking adherence')
-    parser.add_argument("drugname", help='drug to find')
-    parser.add_argument("-d", "--dsih", help='stockpiling percentage, 0-101%%', default=101, type=int)
+    parser.add_argument("dscolumn", help='column name with day supply')
+    parser.add_argument("flagname", help='drugname for created column')
+    parser.add_argument("-d", "--dsih", help='stockpiling percentage, 0-100%%', default=100, type=int)
     parser.add_argument("-g", "--gap", help='gap size, defaults to 14', default=14, type=int)
     parser.add_argument("--delimiter", help='file delimiter, defaults to ","', default=',')
-    parser.add_argument("--count", help='turn counter on', action='store_false')
+    parser.add_argument("--count", help='turn counter on', action='store_true')
     args = parser.parse_args()
-    missing_val = ''
     delim_val = args.delimiter
-    drug = args.drugname
+    infile = open(args.infile)
+    rxfile = open(args.rxfile)
+    outfile = open(args.outputfile, 'w')
+    drug = args.flagname
     adherence_gap = args.gap
     dsih_percent = args.dsih
-    if dsih_percent < 0 or dsih_percent > 101:
-        print "DSIH% must be between 0 and 101"
+    missing_val = ''
+    ### validate arguments
+    if dsih_percent < 0 or dsih_percent > 100:
+        print "DSIH% must be between 0 and 100"
         sys.exit()
-    mainfile = open(args.datafile)
-    dsihfile = dsihFile(args.DSIHfile, dsih_percent)
-    outfile = open(args.outputfile, 'w')
-    if args.count:
-        cnt = Counter()
-    else:
-        cnt = Nothing()
-
-    header = mainfile.readline().rstrip()
+    header = rxfile.readline().rstrip().split(delim_val)
+    if args.dscolumn not in header:
+        print "daysupply column [%s] not found" % (args.dscolumn)
+        sys.exit(-1)
+    dcol = header.index(args.dscolumn)
+    ### header for outfile
+    header = infile.readline().rstrip()
     # date of last refill
     fieldname1 = "%s_stop_%s_%s_Offset" % (drug, adherence_gap, dsih_percent)
     # daysupply of last refill
     fieldname2 = "%s_stop_%s_%s_Supply" % (drug, adherence_gap, dsih_percent)
     outfile.write(header + delim_val + fieldname1 + delim_val + fieldname2 + "\n")
-    linemain = mainfile.readline().rstrip().split(delim_val, 4)
-    linedsih = dsihfile.read()
-    (mid, mdate) = readMain(linemain)
-    (did, ddate, pre, post) = linedsih
-    dates = {}
-    while(len(linemain) == 5 and len(linedsih) == 4):
-        if mid < did:
+    if args.count:
+        cnt = Counter()
+    else:
+        cnt = Nothing()
+    def vals(x):
+        if x[dcol] == '':
+            x[dcol] = 90
+        return [int(x[i]) for i in [2,dcol]]
+    def missingRow(line):
+        return line + delim_val + missing_val + delim_val + missing_val + "\n"
+
+    linemain = infile.readline().rstrip().split(delim_val, 4)
+    rx = rxfile.readline().rstrip().split(delim_val)
+    while len(linemain) == 5:
+        if linemain[0] > rx[0]:
+            (res, rx) = calcDSIH(rx, dsih_percent, vals, rxfile, delim_val, skip=True)
+        elif linemain[0] < rx[0]:
             outfile.write(missingRow(delim_val.join(linemain)))
-            linemain = mainfile.readline().rstrip().split(delim_val, 4)
-            if(len(linemain) == 5):
-                (mid, mdate) = readMain(linemain)
+            linemain = infile.readline().rstrip().split(delim_val, 4)
             cnt.add()
-        elif mid > did:
-            while(mid > did):
-                linedsih = dsihfile.read()
-                if(len(linedsih) != 4):
-                    break
-            (did, ddate, pre, post) = linedsih
         else:
-            matched_id = did
-            while(mid == did and len(linedsih) == 4):
-                dates[ddate]=[pre, post]
-                linedsih = dsihfile.read()
-                if(len(linedsih) == 4):
-                    (did, ddate, pre, post) = linedsih
-            #mid < did unless end of DSIH file was reached -- mid == matched_id AT LEAST the first time
-            while(mid == matched_id):
-                if len(dates) > 0 and mdate in dates.keys():
-                    (dropdate, dropsupply) = calcDropout(mdate, dates, adherence_gap)
-                    res = "%s%s%s" % (dropdate, delim_val, dropsupply)
-                    outfile.write(delim_val.join(linemain) + delim_val + res + "\n")
+            matched_id = rx[0]
+            (res, rx) = calcDSIH(rx, dsih_percent, vals, rxfile, delim_val)
+            while linemain[0] == matched_id:
+                if res is not None:
+                    (dropdate, droppre, dropsupply) = calcDropout(int(linemain[2]), res, adherence_gap)
+                    out = "%s%s%s" % (dropdate, delim_val, dropsupply)
+                    outfile.write(delim_val.join(linemain) + delim_val + out + "\n")
                 else:
                     outfile.write(missingRow(delim_val.join(linemain)))
-                linemain = mainfile.readline().rstrip().split(delim_val, 4)
-                if(len(linemain) == 5):
-                    (mid, mdate) = readMain(linemain)
-                else:
+                linemain = infile.readline().rstrip().split(delim_val, 4)
+                if len(linemain) != 5:
                     break
                 cnt.add()
-            dates.clear()
-    #need to print blank lines for anything left in main file
-    while len(linemain) == 5:
-        outfile.write(missingRow(delim_val.join(linemain)))
-        linemain = mainfile.readline().rstrip().split(delim_val, 4)
-        cnt.add()
-    mainfile.close()
-    dsihfile.close()
+    infile.close()
+    rxfile.close()
     outfile.close()
